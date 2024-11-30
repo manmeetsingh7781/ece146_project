2965558c1d1e7a25c16adfca5e789e2ae0062459
Manmeet Singh <35901948+manmeetsingh7781@users.noreply.github.com>
2024-11-30 14:08:02 -0800

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print("Socket connected")
 
 # hosting it on this machine
-s.bind(("44.233.151.27", 8080))
+s.bind(("35.160.120.126", 8080))
 print("Server host name ", socket.gethostname())
 # queue of 5 clients 
 s.listen(5)
