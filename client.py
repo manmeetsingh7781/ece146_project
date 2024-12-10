fa115ba30f272d1bb5dab03a83f7f16ba49fc3c5
Manmeet Singh <35901948+manmeetsingh7781@users.noreply.github.com>
2024-12-09 17:42:22 -0800

    Add files via upload

--- client.py
+++ client.py
@@ -1,22 +1,103 @@
-import socket
+import socket, threading, tkinter as tk
+from tkinter import scrolledtext
+
+hostname, port = ("singhrasp.ddnsgeek.com", 5000)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+window = tk.Tk()
+window.geometry("500x510")
+window.title("Client")
+window.resizable(False, False)
 
+# scroll view
+chat = scrolledtext.ScrolledText(window, width=150, height=20)
+chat.config(state="disabled")
+
+# allow multiple messages to send 
+def recieve():
+    msg = ""
+    while True:
+        try:
+            msg = s.recv(1024).decode()
+            if not msg:
+                print("Server disconnected")
+                insert_text("Server disconnected")
+            else:
+                print(f"Server: {msg}")
+                # print("Enter a message to send: ")
+                insert_text(f"Server: {msg}")
+        except Exception as e:
+            print("Error: ", e)
+            insert_text(f"Error: {e}")
+
+
+def send():
+    input_msg = input_text_box.get("1.0", tk.END).strip() 
+    try:
+        insert_text(f"You: {input_msg}")
+        # erase the textbox message
+        input_text_box.delete("1.0", tk.END)
+        # send the message
+        s.send(input_msg.encode())
+    except Exception as e:
+        print("Error ", e)
+        insert_text(f"Error: {e}")
+
+# get the value of text box
+def get_text():
+    # get the message
+    input_msg = input_text_box.get("1.0", tk.END).strip()
+    insert_text(f"You: {input_msg}")
+    # erase the textbox message
+    input_text_box.delete("1.0", tk.END)
+    return input_msg
+
+def insert_text(text):
+    # insert message to the scroll text box
+    chat.config(state="normal")
+    chat.insert(tk.END, f"{text}\n")
+    chat.config(state="disabled")
+
+
+label = tk.Label(window, text="Enter a message to send: ")
+label.place(x=0, y=430)
+
+
+# writing text box 
+input_text_box = tk.Text(window, height=2, width=60)
+input_text_box.focus()
+input_text_box.place(x=0, y=450)
+# enter key event
+input_text_box.bind("<Return>", lambda event: send())
+
+send_button = tk.Button(window, text="Send", command=send, height=2)
+send_button.place(x=450, y=450)
+
+exit_button = tk.Button(window, text="Exit", command=lambda: {
+    s.send("quit".encode()),
+    window.destroy(),
+    s.close()
+}, height=2)
+exit_button.place(x=450, y=0)
+
+
+chat.pack()
+
+
+# "singhrasp.ddnsgeek.com"
 # connect to public ip like singhrasp.ddnsgeek.com and give port number
-s.connect(("35.160.120.126", 8080))
-print("Client host name ", socket.gethostname())
+try:
+    s.connect((hostname, port))
+    insert_text(f"Hosting on address and socket {s.getsockname()}")
+    insert_text(f"Client host name {socket.gethostname()}")
+except Exception as e:
+    print("Error: ", e)
+    insert_text(f"Error: {e}")
 
-inputs = "";
 
-while inputs.lower() != "quit":
-    # recieve the message of buffer size 1024
-    msg = s.recv(1024)
-    if msg:
-        # print the message
-        print(msg.decode("utf-8"))
-   
-    inputs = input("Enter a message to send: ")
-    s.send(bytes(inputs, "utf-8"))
+thread_reciever = threading.Thread(target=recieve, daemon=True)
+thread_reciever.start()
+window.mainloop()
 
 # close the connection
 s.close()
--- server.py
+++ server.py
@@ -1,33 +1,79 @@
-import socket, sys
+import socket, threading
 
-# Create a TCP/IP socket
-# AF_INET -> IPv4 
-# SOCK_STREAM -> TCP/IP connections
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("Socket connected")
-
-# hosting it on this machine
-s.bind(("35.160.120.126", 8080))
-print("Server host name ", socket.gethostname())
-# queue of 5 clients 
-s.listen(5)
-print("Socket is listning")
-
+hostname, port = ("localhost", 5000)
 inputs = ""
-clientSocket, address = s.accept()
-print(f"Connection from {address} has been established")
+def handleClient(clientSocket):
+    global inputs;
+    # allow multiple messages to send 
+    def recieve():
+        while True:
+            try:
+                msg = clientSocket.recv(1024).decode('utf-8')
+                if (msg == "quit"):
+                    print("Connection closed!")
+                    
+                    clientSocket.close()    
+                    break
+                if msg:
+                    print("\n===================\n")
+                    print(f"Client: {msg}")
+                    print("\n===================\n")
 
-# what to send to the client
-sent = clientSocket.send(bytes("Welcome to the server!", "utf-8"))
+                    print("Enter a message to send: " )
+            except Exception as e:
+                print("Error: ", e)
+                break
 
-while inputs.lower() != "quit":
-    recieved = clientSocket.recv(1024)
-    if recieved:
-        print("Recieved by host name ", socket.gethostbyaddr(address[0])[0])
-        print(recieved.decode("utf-8"))
 
-    inputs = input("Enter a message to send: ")
-    clientSocket.send(bytes(inputs, "utf-8"))
+    def send():
+        global inputs;
+        while inputs != "quit":
+            inputs = input("Enter a message to send: ")
+            print(f"You: {inputs}")
+            # send the message
+            clientSocket.send(bytes(inputs, "utf-8"))
+            
 
-# close the connection
-clientSocket.close()
+
+    thread_reciever = threading.Thread(target=recieve)
+    thread_reciever.start()
+
+    thread_sender = threading.Thread(target=send)
+    thread_sender.start()
+
+
+    thread_reciever.join()
+    thread_sender.join()
+
+    # close the connection
+    clientSocket.close()
+
+
+def start_server():
+    # Create a TCP/IP socket
+    # AF_INET -> IPv4 
+    # SOCK_STREAM -> TCP/IP connections
+   
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("Socket connected")
+
+    # hosting it on this machine
+    s.bind((hostname, port))
+    print("Server host name ", socket.gethostname())
+
+
+    # queue of 5 clients 
+    s.listen(5)
+    
+
+
+    clientSocket, address = s.accept()
+    print(f"Connection from {address} has been established")
+
+    client_handler = threading.Thread(target=handleClient, args=(clientSocket,))
+    client_handler.start()
+
+    # what to send to the client
+    clientSocket.send(bytes("Welcome to the server!", "utf-8"))
+
+start_server()
